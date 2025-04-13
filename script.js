1960235e9e2cb24b4424cb8f840c55f211796be0
JjavierC <86530758+JjavierC@users.noreply.github.com>
2025-04-13 16:06:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var splide = new Splide('.splide', {
+    type: 'fade',
+    rewind: true,
+    autoplay: true,
+    interval: 3500,
+    pauseOnHover: true,
+    arrows: false,
+    pagination: false,
+    speed: 500,
+  });
+  
+  splide.mount();
+  
